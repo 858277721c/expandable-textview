337b609d92f1aef241f2b8ead1fa503a47c90998
zjun <565061763@qq.com>
2018-09-25 16:41:05 +0800

    demo

--- app/src/main/java/com/sd/expandable_textview/MainActivity.java
+++ app/src/main/java/com/sd/expandable_textview/MainActivity.java
@@ -1,9 +1,11 @@
 package com.sd.expandable_textview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 
     @Override
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        final TextView textView = findViewById(R.id.tv);
+        textView.setText("独立寒秋，湘江北去，橘子洲头。看万山红遍，层林尽染；漫江碧透，百舸争流。鹰击长空，鱼翔浅底，万类霜天竞自由。怅寥廓，问苍茫大地，谁主沉浮？携来百侣曾游，忆往昔峥嵘岁月稠。恰同学少年，风华正茂；书生意气，挥斥方遒。指点江山，激扬文字，粪土当年万户侯。曾记否，到中流击水，浪遏飞舟？");
     }
 }
